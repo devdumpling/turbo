9b0d062c4a8ca226247e5fd217ffa5324663debd
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-06-30 13:45:46 +0200

    ReverseTopological -> AdjacencyMap

--- crates/turbopack-dev/src/chunking_context.rs
+++ crates/turbopack-dev/src/chunking_context.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use indexmap::IndexSet;
 use turbo_tasks::{
-    graph::{GraphTraversal, ReverseTopological},
+    graph::{AdjacencyMap, GraphTraversal},
     primitives::{BoolVc, StringVc},
     TryJoinIterExt, Value,
 };
@@ -352,7 +352,7 @@ async fn get_parallel_chunks<I>(entries: I) -> Result<impl Iterator<Item = Chunk
 where
     I: IntoIterator<Item = ChunkVc>,
 {
-    Ok(ReverseTopological::new()
+    Ok(AdjacencyMap::new()
         .skip_duplicates()
         .visit(entries, |chunk: ChunkVc| async move {
             Ok(chunk
@@ -366,7 +366,7 @@ where
         .await
         .completed()?
         .into_inner()
-        .into_iter())
+        .into_reverse_topological())
 }
 
 async fn get_optimized_chunks<I>(chunks: I) -> Result<ChunksVc>
